cacbc041f034dddfe0319b91b49dd5876f720ac4
Jacob Rief <jacob.rief@gmail.com>
2013-10-02 00:30:11 +0200

    Added possibility to fix margins

--- cmsplugin_bootstrap/cms_plugins.py
+++ cmsplugin_bootstrap/cms_plugins.py
@@ -1,32 +1,53 @@
 from django.forms import widgets
 from django.forms.models import modelform_factory
-from django.utils.translation import ugettext_lazy as _
 from django.utils import simplejson as json
+from django.utils.datastructures import SortedDict
+from django.utils.translation import ugettext_lazy as _
+from django.utils.safestring import mark_safe
 from cms.plugin_pool import plugin_pool
 from cms.plugin_base import CMSPluginBase
 from cmsplugin_bootstrap.models import BootstrapElement
 
 
-class MultipleCheckboxWidget(widgets.CheckboxSelectMultiple):
-    def render(self, name, value, attrs=None):
-        value = value and json.loads(value) or []
-        return super(MultipleCheckboxWidget, self).render(name, value, attrs)
-
-
 class BootstrapPluginBase(CMSPluginBase):
+    class ExtraStylesWidget(widgets.MultiWidget):
+        def __init__(self, **kwargs):
+            wdg_attrs = [{ 'placeholder': 'margin-%s' % d, 'class': 'style-field' } for d in BootstrapElement.style_dirs]
+            margin_widgets = [widgets.TextInput(m) for m in wdg_attrs]
+            super(self.__class__, self).__init__(margin_widgets)
+
+        def decompress(self, value):
+            value = value and json.loads(value) or []
+            value += [None] * (len(BootstrapElement.style_dirs) - len(value))
+            return value
+
+        def value_from_datadict(self, data, files, name):
+            result = [data.get('margin-%s' % d) or None for d in BootstrapElement.style_dirs]
+            return result
+
+        def render(self, name, value, attrs=None):
+            values = self.decompress(value)
+            html = '<p>'
+            for k, d in enumerate(BootstrapElement.style_dirs):
+                html += self.widgets[k].render('margin-%s' % d, values[k], attrs)
+            html += '</p>'
+            return mark_safe(html)
+
     module = 'Bootstrap'
     model = BootstrapElement
+    change_form_template = "cms/plugins/bootstrap/change_form.html"
     render_template = "cms/plugins/bootstrap/generic.html"
     allow_children = True
 
     def __init__(self, *args, **kwargs):
         super(BootstrapPluginBase, self).__init__(*args, **kwargs)
-        choice_widgets = {}
+        extra_widgets = SortedDict()
         if hasattr(self, 'css_classes') and len(self.css_classes) > 1:
-            choice_widgets['class_name'] = widgets.Select(choices=self.css_classes)
-        if hasattr(self, 'extra_classes'):
-            choice_widgets['extra_classes'] = MultipleCheckboxWidget(choices=self.extra_classes)
-        self.form = modelform_factory(BootstrapElement, fields=choice_widgets.keys(), widgets=choice_widgets)
+            extra_widgets['class_name'] = widgets.Select(choices=self.css_classes)
+        if hasattr(self, 'extra_classes_widget'):
+            extra_widgets['extra_classes'] = self.extra_classes_widget
+        extra_widgets['extra_styles'] = self.__class__.ExtraStylesWidget()
+        self.form = modelform_factory(BootstrapElement, fields=extra_widgets.keys(), widgets=extra_widgets)
 
     def save_model(self, request, obj, form, change):
         obj.tag_type = self.tag_type
@@ -57,12 +78,31 @@ plugin_pool.register_plugin(BootstrapRowPlugin)
 
 
 class BootstrapColumnPlugin(BootstrapPluginBase):
+    class ExtraClassesWidget(widgets.MultiWidget):
+        def __init__(self, attrs=None):
+            choices = ((None, 'no offset'),) + tuple(2 * ('offset%s' % o,) for o in range(1, 12))
+            offset_widget = widgets.RadioSelect(choices=choices)
+            super(self.__class__, self).__init__([offset_widget], attrs)
+
+        def decompress(self, value):
+            value = value and json.loads(value) or []
+            value += [None] * (1 - len(value))
+            return value
+
+        def value_from_datadict(self, data, files, name):
+            return [data.get('offset')]
+
+        def render(self, name, value, attrs=None):
+            value = self.decompress(value)
+            html = '<div class="clearfix">' + self.widgets[0].render('offset', value[0], attrs) + '</div>'
+            return mark_safe(html)
+
     name = _("Column container")
     parent_classes = ['BootstrapRowPlugin']
     require_parent = True
     tag_type = 'div'
     css_classes = tuple(2 * ('span%s' % i,) for i in range(1, 13))
-    extra_classes = tuple(2 * ('offset%s' % o,) for o in range(1, 12))
+    extra_classes_widget = ExtraClassesWidget()
 
 plugin_pool.register_plugin(BootstrapColumnPlugin)
 
--- cmsplugin_bootstrap/models.py
+++ cmsplugin_bootstrap/models.py
@@ -8,6 +8,7 @@ class BootstrapElement(CMSPlugin):
     """
     The container to hold additional bootstrap elements.
     """
+    style_dirs = ('top', 'right', 'bottom', 'left')
     cmsplugin_ptr = models.OneToOneField(CMSPlugin, related_name='+', parent_link=True)
     tag_type = models.CharField(verbose_name=_('tag Type'), max_length=50)
     class_name = models.CharField(_("class name"), max_length=50, blank=True, null=True)
@@ -21,11 +22,17 @@ class BootstrapElement(CMSPlugin):
     def css_classes(self):
         css_classes = self.class_name and [self.class_name] or []
         if isinstance(self.extra_classes, list):
-            css_classes += self.extra_classes
+            css_classes += [ec for ec in self.extra_classes if ec]
         return ' '.join(css_classes)
 
     @property
     def inline_styles(self):
-        if isinstance(self.extra_styles, dict):
-            return ' '.join(['{0}: {1};'.format(*s) for s in self.extra_styles.items()])
+        if isinstance(self.extra_styles, list):
+            try:
+                margins = ['margin-{0}: {1};'.format(d, self.extra_styles[k])
+                           for k, d in enumerate(BootstrapElement.style_dirs)
+                                if self.extra_styles[k] is not None]
+                return ' '.join(margins)
+            except IndexError:
+                pass
         return ''
