9fede8a0fe311d6893d7c4774d1c4ec650eaa729
Jacob Rief <jacob.rief@gmail.com>
2014-02-28 08:39:27 +0100

    Using normal layout

--- examples/manage.py
+++ examples/manage.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
-import traceback
 
-sys.path.insert(0, os.path.abspath('./../'))
+sys.path.insert(0, os.path.abspath(os.path.join('..')))
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootstrap3.settings")
-
     from django.core.management import execute_from_command_line
 
-    try:
-        execute_from_command_line(sys.argv)
-    except Exception as exception:
-        # since this response is sent to the PSP, catch errors locally
-        print('%s while performing request' % (exception.__str__()))
-        traceback.print_exc()
-        raise exception
+    execute_from_command_line(sys.argv)
+
