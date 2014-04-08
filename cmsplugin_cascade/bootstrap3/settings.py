ba99706b03d1ae5a04952e3e6dded1c048426e89
Jacob Rief <jacob.rief@gmail.com>
2014-04-08 16:42:14 +0200

    Fixes issue #17

--- cmsplugin_cascade/bootstrap3/settings.py
+++ cmsplugin_cascade/bootstrap3/settings.py
@@ -2,4 +2,16 @@
 from django.conf import settings
 
 CMS_CASCADE_BOOTSTRAP3_BREAKPOINT = getattr(settings, 'CMS_CASCADE_BOOTSTRAP3_BREAKPOINT', 'lg')
-CMS_CASCADE_LEAF_PLUGINS = getattr(settings, 'CMS_CASCADE_LEAF_PLUGINS', ('TextPlugin', 'FilerImagePlugin',))
+
+if not hasattr(settings, 'CMS_CASCADE_LEAF_PLUGINS'):
+    CMS_CASCADE_LEAF_PLUGINS = []
+    try:
+        import djangocms_text_ckeditor
+        CMS_CASCADE_LEAF_PLUGINS.append('TextPlugin')
+    except ImportError:
+        pass
+    try:
+        import filer
+        CMS_CASCADE_LEAF_PLUGINS.append('FilerImagePlugin')
+    except ImportError:
+        pass
