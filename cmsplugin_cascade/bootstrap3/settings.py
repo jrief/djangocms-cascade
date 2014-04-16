d98fde0e961695e3dbc0c6f9f15099ffa3503368
Manuel Weiss <manuel@weiss.io>
2014-04-16 12:26:17 +0100

    Update settings.py
    * use CMS_CASCADE_LEAF_PLUGINS list from settings, if they are there
    * add TextPlugin, FilerImagePlugin if not in list

--- cmsplugin_cascade/bootstrap3/settings.py
+++ cmsplugin_cascade/bootstrap3/settings.py
@@ -2,14 +2,15 @@
 from django.conf import settings
 
 CMS_CASCADE_BOOTSTRAP3_BREAKPOINT = getattr(settings, 'CMS_CASCADE_BOOTSTRAP3_BREAKPOINT', 'lg')
+CMS_CASCADE_LEAF_PLUGINS = getattr(settings, 'CMS_CASCADE_LEAF_PLUGINS', [])
 
-if not hasattr(settings, 'CMS_CASCADE_LEAF_PLUGINS'):
-    CMS_CASCADE_LEAF_PLUGINS = []
+if not 'TextPlugin' in CMS_CASCADE_LEAF_PLUGINS:
     try:
         import djangocms_text_ckeditor
         CMS_CASCADE_LEAF_PLUGINS.append('TextPlugin')
     except ImportError:
         pass
+if not 'FilerImagePlugin' in CMS_CASCADE_LEAF_PLUGINS:
     try:
         import filer
         CMS_CASCADE_LEAF_PLUGINS.append('FilerImagePlugin')
