cdec3a7c97972f7a89c646b930438a7bb53e9c15
Jacob Rief <jacob.rief@gmail.com>
2014-06-20 11:15:24 +0200

    fixed model layout for ImageElement

--- cmsplugin_cascade/image/models.py
+++ cmsplugin_cascade/image/models.py
@@ -1,2 +1,15 @@
 # -*- coding: utf-8 -*-
-from cmsplugin_cascade.models import CascadeModelBase
+from django.utils.translation import ugettext_lazy as _
+from filer.fields.image import FilerImageField
+from cmsplugin_cascade.link.models_base import LinkElementBase
+
+
+class ImageElement(LinkElementBase):
+    """
+    A model class to refer to a Django-Filer images, adding an optional Link and arbitrary context
+    data.
+    """
+    class Meta:
+        app_label = 'cmsplugin_cascade'
+
+    image = FilerImageField(null=True, blank=True, default=None, verbose_name=_("Image"))
