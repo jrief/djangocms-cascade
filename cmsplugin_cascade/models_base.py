e29fe18092aa755c76a51359681c51b35bd331d2
Jacob Rief <jacob.rief@gmail.com>
2014-06-22 21:35:14 +0200

    Refactored for readability

--- cmsplugin_cascade/models_base.py
+++ cmsplugin_cascade/models_base.py
@@ -92,6 +92,6 @@ class CascadeModelBase(CMSPlugin):
         plugins.
         """
         if not hasattr(cls, '_cached_cascade_elements'):
-            setattr(cls, '_cached_cascade_elements',
-                set([p.model for p in plugin_pool.get_all_plugins() if issubclass(p.model, cls)]))
+            cce = set([p.model for p in plugin_pool.get_all_plugins() if issubclass(p.model, cls)])
+            setattr(cls, '_cached_cascade_elements', cce)
         return cls._cached_cascade_elements
