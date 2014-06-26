d1b812f5a67296306f6fd373751c03252db77002
Jacob Rief <jacob.rief@gmail.com>
2014-06-26 18:32:53 +0200

    Added 4 fields for each breakpoint to specify the height of a carousel container

--- cmsplugin_cascade/bootstrap3/carousel.py
+++ cmsplugin_cascade/bootstrap3/carousel.py
@@ -9,7 +9,7 @@ from cmsplugin_cascade.fields import PartialFormField
 from cmsplugin_cascade.forms import ManageChildrenFormMixin
 from cmsplugin_cascade.widgets import NumberInputWidget, MultipleCascadingSizeWidget
 from .plugin_base import BootstrapPluginBase
-from . import settings
+from .settings import CASCADE_BREAKPOINTS_LIST, CMS_CASCADE_TEMPLATE_DIR
 
 
 class CarouselSlidesForm(ManageChildrenFormMixin, ModelForm):
@@ -25,12 +25,12 @@ class CarouselPlugin(BootstrapPluginBase):
     default_css_class = 'carousel'
     default_css_attributes = ('options',)
     parent_classes = ['BootstrapColumnPlugin']
-    render_template = os.path.join('cms', settings.CMS_CASCADE_TEMPLATE_DIR, 'carousel.html')
+    render_template = os.path.join('cms', CMS_CASCADE_TEMPLATE_DIR, 'carousel.html')
     default_inline_styles = {'overflow': 'hidden'}
     fields = ('num_children', 'glossary',)
     DEFAULT_CAROUSEL_ATTRIBUTES = {'data-ride': 'carousel'}
     OPTION_CHOICES = (('slide', _("Animate")), ('pause', _("Pause")), ('wrap', _("Wrap")),)
-    glossary_fields = (
+    GLOSSARY_FIELDS = (
         PartialFormField('interval',
             NumberInputWidget(attrs={'size': '2', 'style': 'width: 4em;', 'min': '1'}),
             label=_("Interval"),
@@ -43,12 +43,18 @@ class CarouselPlugin(BootstrapPluginBase):
             initial=['slide', 'wrap', 'pause'],
             help_text=_("Adjust interval for the carousel."),
         ),
-        PartialFormField('inline_styles',
-            MultipleInlineStylesWidget(['height']),
-            label=_('Inline Styles'),
-            help_text=_('Height of carousel.'),
-        ),
     )
+
+    def get_form(self, request, obj=None, **kwargs):
+        self.glossary_fields = list(self.GLOSSARY_FIELDS)
+        if obj:
+            breakpoints = obj.get_complete_glossary().get('breakpoints', CASCADE_BREAKPOINTS_LIST)
+            self.glossary_fields.append(PartialFormField('container_max_heights',
+                MultipleCascadingSizeWidget(breakpoints),
+                label=_("Carousel heights"),
+                help_text=_("Heights of Carousel in pixels for distinct Bootstrap's breakpoints."),
+            ))
+        return super(CarouselPlugin, self).get_form(request, obj, **kwargs)
 
     @classmethod
     def get_identifier(cls, obj):
@@ -74,6 +80,13 @@ class CarouselPlugin(BootstrapPluginBase):
 
     def save_model(self, request, obj, form, change):
         wanted_children = int(form.cleaned_data.get('num_children'))
+        if obj:
+            # fill all unset maximum heights for this container to meaningful values
+            breakpoints = obj.get_complete_glossary().get('breakpoints', CASCADE_BREAKPOINTS_LIST)
+            max_height = max(obj.glossary['container_max_heights'].values())
+            for bp in breakpoints:
+                if not obj.glossary['container_max_heights'][bp]:
+                    obj.glossary['container_max_heights'][bp] = max_height
         super(CarouselPlugin, self).save_model(request, obj, form, change)
         self.extend_children(obj, wanted_children, CarouselSlidePlugin)
 
