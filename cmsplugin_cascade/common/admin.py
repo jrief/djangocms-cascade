f140b20ec2844edbe15b0ab3461ed20960490ff1
Jacob Rief <jacob.rief@gmail.com>
2014-06-27 23:48:35 +0200

    First ModelAdmin able to borrow form definition from a CMSPlugin

---
+++ cmsplugin_cascade/common/admin.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from cms.plugin_pool import plugin_pool
+from cmsplugin_cascade.widgets import JSONMultiWidget
+from .models import SharedGlossary
+
+
+class SharedGlossaryAdmin(admin.ModelAdmin):
+    list_display = ('identifier', 'plugin_type',)
+
+    class Media:
+        css = {'all': ('admin/css/djangocms-cascade.css',)}
+
+    def get_form(self, request, obj=None, **kwargs):
+        """
+        Creates the form an identifier for the model field. Additionally it adds dynamic fields to
+        edit the content inside the model field `glossary`. The layout and validation for these
+        dynamic fields is borrowed from the corresponding plugin.
+        """
+        try:
+            shared_glossary_fields = plugin_pool.get_plugin(obj.plugin_type).shared_glossary_fields
+        except AttributeError:
+            shared_glossary_fields = ()
+        else:
+            kwargs.update(widgets={'glossary': JSONMultiWidget(shared_glossary_fields)}, labels={'glossary': ''})
+        form = super(SharedGlossaryAdmin, self).get_form(request, obj, **kwargs)
+        # help_text can not be cleared using an empty string in modelform_factory
+        form.base_fields['glossary'].help_text = ''
+        for field in shared_glossary_fields:
+            form.base_fields['glossary'].validators.append(field.run_validators)
+        setattr(form, 'shared_glossary_fields', shared_glossary_fields)
+        return form
+
+admin.site.register(SharedGlossary, SharedGlossaryAdmin)
---
+++ cmsplugin_cascade/common/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from jsonfield.fields import JSONField
+
+
+class SharedGlossary(models.Model):
+    """
+    A model class to hold glossary data shared among different plugins.
+    """
+    class Meta:
+        app_label = 'cmsplugin_cascade'
+
+    plugin_type = models.CharField('plugin_name', max_length=50, db_index=True, editable=False)
+    identifier = models.CharField(_("Identifier"), max_length=50)
+    glossary = JSONField(null=True, blank=True, default={})
