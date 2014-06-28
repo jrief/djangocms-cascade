6ef17027d0f6bc3cacc52792372968d8169064fa
Jacob Rief <jacob.rief@gmail.com>
2014-06-29 00:44:35 +0200

    identifier is unique

--- cmsplugin_cascade/common/models.py
+++ cmsplugin_cascade/common/models.py
@@ -12,5 +12,8 @@ class SharedGlossary(models.Model):
         app_label = 'cmsplugin_cascade'
 
     plugin_type = models.CharField('plugin_name', max_length=50, db_index=True, editable=False)
-    identifier = models.CharField(_("Identifier"), max_length=50)
+    identifier = models.CharField(_("Identifier"), max_length=50, unique=True)
     glossary = JSONField(null=True, blank=True, default={})
+
+    def __str__(self):
+        return self.identifier
