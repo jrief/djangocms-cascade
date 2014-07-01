d73bd666e3be952027bd7750007f2a42040d3745
Jacob Rief <jacob.rief@gmail.com>
2014-07-01 16:12:13 +0200

    Fixed empty field issue for Select2 input fields

--- cmsplugin_cascade/link/fields.py
+++ cmsplugin_cascade/link/fields.py
@@ -7,7 +7,7 @@ else:
 
 
 class PageSearchField(AutoModelSelect2Field):
-    empty_values = []
+    empty_value = []
     search_fields = ['title_set__title__icontains', 'title_set__menu_title__icontains', 'title_set__slug__icontains']
 
     def security_check(self, request, *args, **kwargs):
