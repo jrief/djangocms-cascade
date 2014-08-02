4d282fec6e1310e057989a6d2fb8b2e4aeaba3ae
Jacob Rief <jacob.rief@gmail.com>
2014-08-02 16:37:55 +0200

    Frontend for PicturePlugin working again

--- cmsplugin_cascade/static/admin/js/cascade-pictureplugin.js
+++ cmsplugin_cascade/static/admin/js/cascade-pictureplugin.js
@@ -1,14 +1,18 @@
 django.jQuery(function($) {
 	'use strict';
+	django.cascade = django.cascade || {};
 
 	// reorganize layout to be more intuitive to use
+	// move Radio boxes for Link Target 
 	$('#id_glossary_target').parents('.glossary-widget').before($('#id_link_type').parents('.form-row'));
+	// move the select box for Shared Glossary just before 'Image Shapes'
+	$('#id_glossary_image-shapes').parents('.glossary-widget').before($('#id_shared_glossary').parents('.form-row'));
 
 	// if checkbox Image Shapes: Responsive is active, replace 'Image Size' against 'Container Height'
 	(function() {
 		var $image_responsive = $('#id_glossary_image-shapes_0'),
-		$glossary_responsive = $('#id_glossary_responsive-height').parents('.glossary-widget'),
-		$glossary_static = $('#id_glossary_image-size').parents('.glossary-widget');
+			$glossary_responsive = $('#id_glossary_responsive-height').parents('.glossary-widget'),
+			$glossary_static = $('#id_glossary_image-size').parents('.glossary-widget');
 		$image_responsive.change(function(evt) {
 			toggleResponsive(evt.target.checked);
 		});
@@ -23,53 +27,32 @@ django.jQuery(function($) {
 				$glossary_static.show();
 			}
 		}
-	})();
 
-	// if shared glossary is set, hide 'Image Size' and 'Resize Optinon'
-	(function() {
-		var $shared_glossary = $('#id_shared_glossary'),
-			$save_as_identifier = $('#id_save_as_identifier');
-		if ($shared_glossary.children('option').length > 1) {
-			// move the select box for Shared Glossary just below 'Image Shapes'
-			$('#id_glossary_image-shapes').parents('.glossary-widget').after($shared_glossary.parents('.form-row'));
-			$shared_glossary.change(function(evt) {
-				toggleSharedGlossary($(this).children('option:selected'));
-			});
-			toggleSharedGlossary($shared_glossary.children('option:selected'));
-		} else {
-			// remove the select box, since it doesn't contain any options
-			$shared_glossary.parents('.form-row').remove();
-		}
-
-		function toggleSharedGlossary($option) {
+		django.cascade.toggleSharedGlossary = function($option) {
 			var glossary = $option.data('glossary');
+			console.log(glossary);
 			if (glossary) {
-				$('#imageelement_form .glossary_image-size input').each(function(idx, elem) {
-					var name = $(elem).attr('name').replace('image-size-', '');
-					$(elem).val(glossary['image-size'][name]).prop('disabled', 'disabled');
-				});
-				$('#id_glossary_responsive-height').val(glossary['responsive-height']).prop('disabled', 'disabled');
-				$('#imageelement_form .glossary_resize-options input').each(function(idx, elem) {
-					$(elem).prop('checked', glossary['resize-options'].indexOf($(elem).val()) >=0);
+				$('#id_glossary_image-shapes input').each(function(idx, elem) {
+					$(elem).prop('checked', glossary['image-shapes'].indexOf($(elem).val()) >= 0);
 					$(elem).prop('disabled', 'disabled');
 				});
-				$('#imageelement_form .form-row.field-save_shared_glossary').hide();
+				toggleResponsive($image_responsive.prop('checked'));
+				$('#id_glossary_responsive-height').val(glossary['responsive-height']).prop('disabled', 'disabled');
+				$('#id_glossary_image-size[name$="width"]').val(glossary['image-size']['width']).prop('disabled', 'disabled');
+				$('#id_glossary_image-size[name$="height"]').val(glossary['image-size']['height']).prop('disabled', 'disabled');
+				$('#id_glossary_resize-options input').each(function(idx, elem) {
+					$(elem).prop('checked', glossary['resize-options'].indexOf($(elem).val()) >= 0);
+					$(elem).prop('disabled', 'disabled');
+				});
+				$('#pictureelement_form .field-save_shared_glossary.field-save_as_identifier').hide();
 			} else {
-				$('#imageelement_form .glossary_image-size input').prop('disabled', '');
-				$('#imageelement_form .glossary_resize-options input').prop('disabled', '');
-				$('#imageelement_form .form-row.field-save_shared_glossary').show();
+				$('#id_glossary_image-shapes input').prop('disabled', '');
+				$('#id_glossary_responsive-height').prop('disabled', '');
+				$('#id_glossary_image-size[name$="width"]').prop('disabled', '');
+				$('#id_glossary_image-size[name$="height"]').prop('disabled', '');
+				$('#id_glossary_resize-options input').prop('disabled', '');
+				$('#pictureelement_form .field-save_shared_glossary.field-save_as_identifier').show();
 			}
 		}
-
-		// handle checkbox field 'Remember image size as'
-		$save_as_identifier.prop('disabled', 'disabled');
-		$('#id_save_shared_glossary').change(function(evt) {
-			if (evt.target.checked) {
-				$save_as_identifier.prop('disabled', '');
-			} else {
-				$save_as_identifier.prop('disabled', 'disabled');
-				$save_as_identifier.val('');
-			}
-		});
 	})();
 });
