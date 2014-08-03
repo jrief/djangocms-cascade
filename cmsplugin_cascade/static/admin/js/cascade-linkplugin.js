9a3d1d2d0bfe8245b31ffaaee4bbc5b0d51c2918
Jacob Rief <jacob.rief@gmail.com>
2014-08-03 17:48:51 +0200

    Refactoring to be ctor-function instead of object

--- cmsplugin_cascade/static/admin/js/cascade-linkplugin.js
+++ cmsplugin_cascade/static/admin/js/cascade-linkplugin.js
@@ -3,47 +3,52 @@ django.cascade = django.cascade || {};
 django.jQuery(function($) {
 	'use strict';
 
-	var $link_type = $("#id_link_type"),
-		$field_cmspage = $(".form-row.field-link_type .field-box.field-cms_page"),
-		$field_exturl = $(".form-row.field-link_type .field-box.field-ext_url"),
-		$field_mailto = $(".form-row.field-link_type .field-box.field-mail_to"),
-		$link_target = $(".glossary-widget.glossary_target");
+	django.cascade.LinkPlugin = {
+		$link_type: $("#id_link_type"),
+		$field_cmspage: $(".form-row.field-link_type .field-box.field-cms_page"),
+		$field_exturl: $(".form-row.field-link_type .field-box.field-ext_url"),
+		$field_mailto: $(".form-row.field-link_type .field-box.field-mail_to"),
+		$link_target: $(".glossary-widget.glossary_target"),
+		init: function() {
+			var self = this;
 
-	// move the select box for Shared Glossary just before 'Image Shapes'
-	$('.field-link_content').after($('.field-shared_glossary'));
-
-	$link_type.change(function(evt) {
-		toggleLinkTypes(evt.target.value);
-	});
-	toggleLinkTypes($link_type.val());
-
-
-	function toggleLinkTypes(linkType) {
-		switch(linkType) {
-		case 'cmspage':
-			$field_cmspage.show();
-			$field_exturl.hide();
-			$field_mailto.hide();
-			$link_target.show();
-			break;
-		case 'exturl':
-			$field_cmspage.hide();
-			$field_exturl.show();
-			$field_mailto.hide();
-			$link_target.show();
-			break;
-		case 'email':
-			$field_cmspage.hide();
-			$field_exturl.hide();
-			$field_mailto.show();
-			$link_target.show();
-			break;
-		default:
-			$field_cmspage.hide();
-			$field_exturl.hide();
-			$field_mailto.hide();
-			$link_target.hide();
-			break;
+			// register event handler on changing link type select box
+			this.$link_type.change(function(evt) {
+				self.toggleLinkTypes(evt.target.value);
+			});
+			this.toggleLinkTypes(this.$link_type.val());
+		},
+		toggleLinkTypes: function(linkType) {
+			switch(linkType) {
+			case 'cmspage':
+				this.$field_cmspage.show();
+				this.$field_exturl.hide();
+				this.$field_mailto.hide();
+				this.$link_target.show();
+				break;
+			case 'exturl':
+				this.$field_cmspage.hide();
+				this.$field_exturl.show();
+				this.$field_mailto.hide();
+				this.$link_target.show();
+				break;
+			case 'email':
+				this.$field_cmspage.hide();
+				this.$field_exturl.hide();
+				this.$field_mailto.show();
+				this.$link_target.show();
+				break;
+			default:
+				this.$field_cmspage.hide();
+				this.$field_exturl.hide();
+				this.$field_mailto.hide();
+				this.$link_target.hide();
+				break;
+			}
 		}
-	}
+	};
+
+	$(document).ready(function() {
+		Object.create(django.cascade.LinkPlugin).init();
+	});
 });
