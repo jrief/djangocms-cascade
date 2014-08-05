27d177fa9dcf8270c51ea519328fe0d993e533a7
Jacob Rief <jacob.rief@gmail.com>
2014-08-05 23:48:31 +0200

    Adopted loader to include plugins from external apps

--- cmsplugin_cascade/cms_plugins.py
+++ cmsplugin_cascade/cms_plugins.py
@@ -7,17 +7,17 @@ from django.utils.importlib import import_module
 for module in getattr(settings, 'CMS_CASCADE_PLUGINS'):
     try:
         # if a module was specified, load all plugins in module settings
-        module_settings = import_module('.{0}.settings'.format(module), 'cmsplugin_cascade')
+        module_settings = import_module('{0}.settings'.format(module))
         module_plugins = getattr(module_settings, 'CMS_CASCADE_PLUGINS', [])
         for p in module_plugins:
             try:
-                import_module('.{0}.{1}'.format(module, p), 'cmsplugin_cascade')
+                import_module('{0}.{1}'.format(module, p))
             except ImportError:
                 raise ImproperlyConfigured("Plugin {0} as specified in {1}.settings.CMS_CASCADE_PLUGINS could not be loaded".format(p, module))
     except ImportError:
         try:
             # otherwise try with cms_plugins in the named module
-            import_module('.{0}.cms_plugins'.format(module), 'cmsplugin_cascade')
+            import_module('{0}.cms_plugins'.format(module))
         except ImportError:
             # otherwise just use the named module as plugin
-            import_module('.{0}'.format(module), 'cmsplugin_cascade')
+            import_module('{0}'.format(module))
