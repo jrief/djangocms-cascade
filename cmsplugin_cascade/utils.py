56f34dbcd81670491d26887299cd5398d3d926b8
Jacob Rief <jacob.rief@gmail.com>
2014-08-06 12:08:26 +0200

    Readability counts!

--- cmsplugin_cascade/utils.py
+++ cmsplugin_cascade/utils.py
@@ -3,24 +3,30 @@ from cmsplugin_cascade import settings
 
 
 def remove_duplicates(lst):
+    """
+    Emulate what a Python ``set()`` does, but keeping the element's order.
+    """
     dset = set()
     return [l for l in lst if l not in dset and not dset.add(l)]
 
 
 def resolve_dependencies(filenames):
     """
-    Given a single filename or a list of files and a mapping of dependencies,
-    see ``settings.CASCADE_PLUGIN_DEPENDENCIES`` for details, return a list of other files
-    resolving the dependency. The list is ordered, so that files having no further dependencies
-    come first.
-    Use this function to automatically resolve JavaScript dependencies in the ``Media`` subclasses.
+    Given a filename literal or a list of filenames and a mapping of dependencies (use
+    ``settings.CASCADE_PLUGIN_DEPENDENCIES`` to check for details), return a list of other files
+    resolving the dependency. The returned list is ordered, so that files having no further
+    dependency come as first element and the passed in filenames come as the last element.
+    Use this function to automatically resolve dependencies of CSS and JavaScript files in the
+    ``Media`` subclasses.
     """
     dependencies = []
     if isinstance(filenames, (list, tuple)):
         for filename in filenames:
             dependencies.extend(resolve_dependencies(filename))
-        return dependencies
-    dependency_list = getattr(settings, 'CASCADE_PLUGIN_DEPENDENCIES', {}).get(filenames)
-    if dependency_list:
-        dependencies.extend(resolve_dependencies(dependency_list))
-    return remove_duplicates(dependencies) + [filenames]
+    else:
+        filename = filenames
+        dependency_list = getattr(settings, 'CASCADE_PLUGIN_DEPENDENCIES', {}).get(filename)
+        if dependency_list:
+            dependencies.extend(resolve_dependencies(dependency_list))
+        dependencies.append(filename)
+    return remove_duplicates(dependencies)
