1c33a4acf51975f0ad7dc89eb7f1ee3efccdda3a
Jacob Rief <jacob.rief@gmail.com>
2014-12-04 17:30:45 +0100

    Don't throw an exception, if glossary does not contain expected data

--- cmsplugin_cascade/bootstrap3/utils.py
+++ cmsplugin_cascade/bootstrap3/utils.py
@@ -13,10 +13,13 @@ def reduce_breakpoints(plugin, field_name):
     complete_glossary = plugin.get_parent_instance().get_complete_glossary()
     if 'breakpoints' not in complete_glossary:
         return
-    # find the glossary_field named field_name and restrict its breakpoint to the available ones
-    widget = [f for f in plugin.glossary_fields if f.name == field_name][0].widget
+    try:
+        # find the glossary_field named field_name and restrict its breakpoint to the available ones
+        widget = [f for f in plugin.glossary_fields if f.name == field_name][0].widget
+    except IndexError:
+        return
     if not isinstance(widget, widgets.MultiWidget):
-        raise ValueError('Widget for glossary_field {0} does not a multiple values')
+        raise ValueError('Widget for glossary_field {0} is not a multiple value field')
     temp = [(l, widget.widgets[k]) for k, l in enumerate(widget.labels) if l in complete_glossary['breakpoints']]
     widget.labels, widget.widgets = (list(t) for t in zip(*temp))
 
