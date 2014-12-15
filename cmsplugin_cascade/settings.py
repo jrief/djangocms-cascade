ac792dae899ab438bd5c1d7732f2719fc929d721
Jacob Rief <jacob.rief@gmail.com>
2014-12-15 19:05:14 +0100

    hack to enforce ordering

--- cmsplugin_cascade/settings.py
+++ cmsplugin_cascade/settings.py
@@ -4,7 +4,7 @@ from django.conf import settings
 CASCADE_PLUGIN_DEPENDENCIES = {
     'cascade/js/ring.js': 'cascade/js/underscore.js',
     'cascade/js/admin/sharableglossary.js': 'cascade/js/ring.js',
-    'cascade/js/admin/linkpluginbase.js': 'cascade/js/ring.js',
+    'cascade/js/admin/linkpluginbase.js': ('cascade/js/admin/sharableglossary.js',),  # TODO: only required to enforce order
     'cascade/js/admin/linkplugin.js': ('cascade/js/admin/linkpluginbase.js',),
     'cascade/js/admin/imageplugin.js': ('cascade/js/admin/linkpluginbase.js',),
     'cascade/js/admin/pictureplugin.js': ('cascade/js/admin/linkpluginbase.js',),
