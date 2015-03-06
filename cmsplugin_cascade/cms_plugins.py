657578494d1c8428b8c6d9fa00cf34847e3498f3
Jacob Rief <jacob.rief@gmail.com>
2015-03-06 11:04:16 +0100

    Simplified format strings

--- cmsplugin_cascade/cms_plugins.py
+++ cmsplugin_cascade/cms_plugins.py
@@ -8,18 +8,18 @@ from django.utils.importlib import import_module
 for module in getattr(settings, 'CMSPLUGIN_CASCADE_PLUGINS', []):
     try:
         # if a module was specified, load all plugins in module settings
-        module_settings = import_module('{0}.settings'.format(module))
+        module_settings = import_module('{}.settings'.format(module))
         module_plugins = getattr(module_settings, 'CASCADE_PLUGINS', [])
         for p in module_plugins:
             try:
-                import_module('{0}.{1}'.format(module, p))
+                import_module('{}.{}'.format(module, p))
             except ImportError as err:
-                raise ImproperlyConfigured("Plugin {0} as specified in {1}.settings.CMSPLUGIN_CASCADE_PLUGINS "
-                                           "could not be loaded: {2}".format(p, module, err.message))
+                msg = "Plugin {} as specified in {}.settings.CMSPLUGIN_CASCADE_PLUGINS could not be loaded: {}"
+                raise ImproperlyConfigured(msg.format(p, module, err.message))
     except ImportError:
         try:
             # otherwise try with cms_plugins in the named module
-            import_module('{0}.cms_plugins'.format(module))
+            import_module('{}.cms_plugins'.format(module))
         except ImportError:
             # otherwise just use the named module as plugin
-            import_module('{0}'.format(module))
+            import_module('{}'.format(module))
