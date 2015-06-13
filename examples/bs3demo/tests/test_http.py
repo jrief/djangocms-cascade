3b574ca16227561d7b589578f243692687c8d602
Fran <fran@changeset.hr>
2015-06-13 14:33:47 +0200

    Make test compatible with Django 1.6

--- examples/bs3demo/tests/test_http.py
+++ examples/bs3demo/tests/test_http.py
@@ -3,10 +3,12 @@ from __future__ import unicode_literals
 import json
 from django.conf import settings
 from django.contrib import admin
-from django.test import modify_settings
+from django.test.utils import override_settings
 from cms.models import Page
 from cms.utils.compat.dj import is_installed
 from cms.test_utils.testcases import CMSTestCase, URL_CMS_PAGE_ADD, URL_CMS_PLUGIN_ADD
+
+APPS_WITHOUT_REVERSION = [app for app in settings.INSTALLED_APPS if app != 'reversion']
 
 
 class ContainerPluginTest(CMSTestCase):
@@ -66,7 +68,7 @@ class ContainerPluginTest(CMSTestCase):
         )
         self.assertEqual(response.status_code, 200)
 
-    @modify_settings(INSTALLED_APPS={'remove': 'reversion'})
+    @override_settings(INSTALLED_APPS=APPS_WITHOUT_REVERSION)
     def test_without_reversion(self):
         self.assertFalse(is_installed('reversion'))
         self._create_and_configure_a_container_plugin()
