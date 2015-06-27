67ed6f19b0f0804606af872f3a237b43cda3f509
Jacob Rief <jacob.rief@gmail.com>
2015-06-27 15:34:31 +0200

    Nobody uses 960.gs anymore

--- cmsplugin_cascade/gs960/settings.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf import settings
-
-CMS_CASCADE_PLUGINS = ('grid',)
-CMS_CASCADE_LEAF_PLUGINS = getattr(settings, 'CMS_CASCADE_LEAF_PLUGINS', ('TextPlugin', 'FilerImagePlugin',))
