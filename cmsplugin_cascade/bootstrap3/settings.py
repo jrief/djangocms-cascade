d7705ae835c30e1e2220e9678382dcbc69f26bd6
Jacob Rief <jacob.rief@gmail.com>
2015-08-18 10:08:47 +0200

    Added plugin to render secondary menus

--- cmsplugin_cascade/bootstrap3/settings.py
+++ cmsplugin_cascade/bootstrap3/settings.py
@@ -5,6 +5,8 @@ from django.utils.translation import ugettext_lazy as _
 from cmsplugin_cascade.settings import CASCADE_PLUGINS_WITH_EXTRA_RENDER_TEMPLATES
 
 CASCADE_PLUGINS = ('buttons', 'carousel', 'accordion', 'container', 'image', 'picture', 'panel')
+if 'cms_bootstrap3' in settings.INSTALLED_APPS:
+    CASCADE_PLUGINS += ('secondary_menu',)
 
 CASCADE_BOOTSTRAP3_BREAKPOINTS = (
     ('xs', (768, 'mobile-phone', _("mobile phones"), 750)),
@@ -30,4 +32,8 @@ CASCADE_PLUGINS_WITH_EXTRA_RENDER_TEMPLATES.setdefault('CarouselPlugin', (  # @U
 CASCADE_PLUGINS_WITH_EXTRA_RENDER_TEMPLATES.setdefault('AccordionPlugin', (  # @UndefinedVariable
     ('cascade/bootstrap3/accordion.html', _("default")),
     ('cascade/bootstrap3/angular-ui/accordion.html', "angular-ui"),
+))
+CASCADE_PLUGINS_WITH_EXTRA_RENDER_TEMPLATES.setdefault('BootstrapSecondaryMenuPlugin', (  # @UndefinedVariable
+    ('cascade/bootstrap3/secmenu-list-group.html', _("default")),
+    ('cascade/bootstrap3/secmenu-unstyled-list.html', _("unstyled")),
 ))
