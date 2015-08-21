645cd536aa7de5523cf4280b720cb02f2ba775ca
Julien Blanc <julien.blanc@nmc-company.fr>
2015-08-21 10:19:32 +0200

    Allow Picture / Image as child of Panel
    * So that it is possible to put an image inside a panel

--- cmsplugin_cascade/bootstrap3/image.py
+++ cmsplugin_cascade/bootstrap3/image.py
@@ -48,7 +48,7 @@ class BootstrapImagePlugin(LinkPluginBase):
     name = _("Image")
     model_mixins = (ImagePropertyMixin, LinkElementMixin,)
     module = 'Bootstrap'
-    parent_classes = ['BootstrapColumnPlugin']
+    parent_classes = ['BootstrapColumnPlugin','BootstrapPanelPlugin']
     require_parent = True
     allow_children = False
     raw_id_fields = ('image_file',)
