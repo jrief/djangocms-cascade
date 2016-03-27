b8a5e8ea7c1c4dbe2e62217d17d10ffa9a7e3932
Jacob Rief <jacob.rief@gmail.com>
2016-03-27 19:03:57 +0200

    added migration for proxy models

---
+++ cmsplugin_cascade/migrations/0007_headingpluginmodel_horizontalrulepluginmodel_segmentpluginmodel_simplewrapperpluginmodel.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import shop.cascade.segmentation
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cmsplugin_cascade', '0006_bootstrapgallerypluginmodel'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='HeadingPluginModel',
+            fields=[
+            ],
+            options={
+                'proxy': True,
+            },
+            bases=('cmsplugin_cascade.cascadeelement',),
+        ),
+        migrations.CreateModel(
+            name='HorizontalRulePluginModel',
+            fields=[
+            ],
+            options={
+                'proxy': True,
+            },
+            bases=('cmsplugin_cascade.cascadeelement',),
+        ),
+        migrations.CreateModel(
+            name='SegmentPluginModel',
+            fields=[
+            ],
+            options={
+                'proxy': True,
+            },
+            bases=(shop.cascade.segmentation.EmulateCustomerModelMixin, 'cmsplugin_cascade.cascadeelement'),
+        ),
+        migrations.CreateModel(
+            name='SimpleWrapperPluginModel',
+            fields=[
+            ],
+            options={
+                'proxy': True,
+            },
+            bases=('cmsplugin_cascade.cascadeelement',),
+        ),
+    ]
