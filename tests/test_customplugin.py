4312a901f98b2f986cced16d2b4be18e33c2f3a2
Jacob Rief <jacob.rief@gmail.com>
2016-04-04 23:22:14 +0200

    fixed expection for app_label

--- tests/test_customplugin.py
+++ tests/test_customplugin.py
@@ -7,12 +7,6 @@ from cmsplugin_cascade.plugin_base import CascadePluginBase
 class CustomPlugin(CascadePluginBase):
     name = 'Custom Element'
     render_template = 'cascade/generic/naked.html'
-    app_label = 'foo'
-
-
-class CustomPluginWithoutAppLabel(CascadePluginBase):
-    name = 'Custom Element'
-    render_template = 'cascade/generic/naked.html'
 
 
 class CustomPluginTest(TestCase):
@@ -21,7 +15,4 @@ class CustomPluginTest(TestCase):
         plugin_pool.register_plugin(CustomPlugin)
 
     def test_proxy_model_has_correct_app_label(self):
-        self.assertEqual(CustomPlugin.model._meta.app_label,
-                         'foo')
-        self.assertEqual(CustomPluginWithoutAppLabel.model._meta.app_label,
-                         'tests')
+        self.assertEqual(CustomPlugin.model._meta.app_label, 'cascade_dummy')
