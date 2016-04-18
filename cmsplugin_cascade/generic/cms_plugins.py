fe1264659913a138de92d1e09d7c3df31b8addd3
Jacob Rief <jacob.rief@gmail.com>
2016-04-18 23:26:25 +0200

    added CustomSnippetPlugin
    # Conflicts:
    #	cmsplugin_cascade/generic/cms_plugins.py
    
    # Conflicts:
    #	cmsplugin_cascade/generic/cms_plugins.py

--- cmsplugin_cascade/generic/cms_plugins.py
+++ cmsplugin_cascade/generic/cms_plugins.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+from django.conf import settings
 from django.forms import widgets
 from django.utils.html import format_html
 from django.utils.translation import ugettext_lazy as _
@@ -80,3 +82,25 @@ class HeadingPlugin(CascadePluginBase):
         return context
 
 plugin_pool.register_plugin(HeadingPlugin)
+
+
+class CustomSnippetPlugin(TransparentMixin, CascadePluginBase):
+    """
+    Allows to add a customized template anywhere. This plugins will be registered only if the
+    project added a template using the configuration setting 'plugins_with_extra_render_templates'.
+    """
+    name = _("Custom Snippet")
+    parent_classes = None
+    require_parent = False
+    allow_children = True
+    alien_child_classes = True
+    render_template_choices = dict(settings.CMSPLUGIN_CASCADE['plugins_with_extra_render_templates'].get('CustomSnippetPlugin', ()))
+
+    @classmethod
+    def get_identifier(cls, instance):
+        render_template = instance.glossary.get('render_template')
+        if render_template:
+            return format_html('{}', cls.render_template_choices.get(render_template))
+
+if CustomSnippetPlugin.render_template_choices:
+    plugin_pool.register_plugin(CustomSnippetPlugin)
