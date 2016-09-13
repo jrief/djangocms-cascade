dd0fad80353265d0e4473c6df944180c6d3905b9
Jacob Rief <jacob.rief@gmail.com>
2016-09-13 23:56:50 +0200

    modified defaults for some Plugin’s extra_fields

--- cmsplugin_cascade/settings.py
+++ cmsplugin_cascade/settings.py
@@ -44,14 +44,17 @@ CMSPLUGIN_CASCADE['dependencies'].update(orig_config.get('dependencies', {}))
 if 'cmsplugin_cascade.extra_fields' in settings.INSTALLED_APPS:
     CMSPLUGIN_CASCADE['plugins_with_extra_fields'] = {
         'BootstrapButtonPlugin': PluginExtraFieldsConfig(),
-        'BootstrapContainerPlugin': PluginExtraFieldsConfig(),
         'BootstrapRowPlugin': PluginExtraFieldsConfig(),
         'BootstrapJumbotronPlugin': PluginExtraFieldsConfig(inline_styles={
             'extra_fields:Paddings': ['padding-top', 'padding-bottom'],
-            'extra_units:Paddings': 'px,em'}, allow_override=False),
+            'extra_units:Paddings': 'px,em'}),
         'SimpleWrapperPlugin': PluginExtraFieldsConfig(),
-        'HeadingPlugin': PluginExtraFieldsConfig(),
-        'HorizontalRulePlugin': PluginExtraFieldsConfig(),
+        'HeadingPlugin': PluginExtraFieldsConfig(inline_styles={
+            'extra_fields:Paddings': ['margin-top', 'margin-right', 'margin-bottom', 'margin-left'],
+            'extra_units:Paddings': 'px,em'}, allow_override=False),
+        'HorizontalRulePlugin': PluginExtraFieldsConfig(inline_styles={
+            'extra_fields:Paddings': ['margin-top', 'margin-bottom'],
+            'extra_units:Paddings': 'px,em'}, allow_override=False),
     }
     CMSPLUGIN_CASCADE['plugins_with_extra_fields'].update(
         orig_config.get('plugins_with_extra_fields', {}))
