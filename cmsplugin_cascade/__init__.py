73a38196b866ff48cba917b179d9eb4ca086320e
Jacob Rief <jacob.rief@gmail.com>
2016-09-18 11:48:20 +0200

    Bump to version 0.10.0

--- cmsplugin_cascade/__init__.py
+++ cmsplugin_cascade/__init__.py
@@ -19,6 +19,6 @@ Release logic:
 12. git commit -m 'Start with <version>'
 13. git push
 """
-__version__ = "0.10.dev"
+__version__ = "0.10.0"
 
 default_app_config = 'cmsplugin_cascade.apps.CascadeConfig'
