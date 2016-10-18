4a64f14f0365dc138fed1c621af9d951687df30a
Anna Damm <anna.damm@netdoktor.de>
2016-10-18 18:02:52 +0200

    Fixed database migration failing on multiple database setup

--- cmsplugin_cascade/migrations/0010_refactor_heading.py
+++ cmsplugin_cascade/migrations/0010_refactor_heading.py
@@ -6,7 +6,7 @@ from django.db import migrations
 
 def forwards(apps, schema_editor):
     CascadeElement = apps.get_model('cmsplugin_cascade', 'CascadeElement')
-    for element in CascadeElement.objects.filter(plugin_type='HeadingPlugin'):
+    for element in CascadeElement.objects.using(schema_editor.connection.alias).filter(plugin_type='HeadingPlugin'):
         head_size = element.glossary.pop('head_size', None)
         if head_size:
             element.glossary['tag_type'] = 'h{}'.format(head_size)
@@ -15,7 +15,7 @@ def forwards(apps, schema_editor):
 
 def backwards(apps, schema_editor):
     CascadeElement = apps.get_model('cmsplugin_cascade', 'CascadeElement')
-    for element in CascadeElement.objects.filter(plugin_type='HeadingPlugin'):
+    for element in CascadeElement.objects.using(schema_editor.connection.alias).filter(plugin_type='HeadingPlugin'):
         tag_type = element.glossary.pop('tag_type', None)
         if tag_type and len(tag_type) == 2:
             element.glossary['head_size'] = tag_type[1]
