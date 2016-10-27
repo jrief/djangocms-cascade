8db6bdb4cf72cb9ef37147ffbc8ad4b4ff637e44
Jacob Rief <jacob.rief@gmail.com>
2016-10-27 22:05:03 +0200

    declare correct Python versions

--- setup.py
+++ setup.py
@@ -23,7 +23,8 @@ CLASSIFIERS = [
     'Programming Language :: Python',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
 ]
 
 setup(
