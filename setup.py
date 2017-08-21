d0e066481725943e04d7c728dfa2229a9b2a8747
Jacob Rief <jacob.rief@gmail.com>
2017-08-21 12:32:29 +0200

    mark as Python-3.6 ready

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
 ]
 
 setup(
