b4f700a27410ec5efca459e76afce61adca0eb58
Jacob Rief <jacob.rief@gmail.com>
2017-12-16 17:45:25 +0100

    Added migration for attribute color in FramedIcon

---
+++ cmsplugin_cascade/migrations/0018_iconfont_color.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+from django.utils.six import string_types
+from cmsplugin_cascade.models import CascadeElement
+
+
+def forwards(apps, schema_editor):
+    for cascade_element in CascadeElement.objects.all():
+        if cascade_element.plugin_type != 'FramedIconPlugin':
+            continue
+
+        color = cascade_element.glossary.get('color')
+        if isinstance(color, string_types):
+            cascade_element.glossary['color'] = ('', color)
+            cascade_element.save()
+        shared = cascade_element.shared_glossary
+        if shared:
+            color = shared.glossary.get('color')
+            if isinstance(color, string_types):
+                shared.glossary['color'] = ('', color)
+                shared.save()
+
+
+def backwards(apps, schema_editor):
+    for cascade_element in CascadeElement.objects.all():
+        if cascade_element.plugin_type != 'FramedIconPlugin':
+            continue
+
+        color = cascade_element.glossary.get('color')
+        if isinstance(color, (list, tuple)):
+            cascade_element.glossary['color'] = color[1]
+            cascade_element.save()
+        shared = cascade_element.shared_glossary
+        if shared:
+            color = shared.glossary.get('color')
+            if isinstance(color, (list, tuple)):
+                shared.glossary['color'] = color[1]
+                shared.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cmsplugin_cascade', '0017_fake_proxy_models'),
+    ]
+
+    operations = [
+        migrations.RunPython(forwards, reverse_code=backwards),
+    ]
