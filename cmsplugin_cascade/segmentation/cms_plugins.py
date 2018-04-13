5d12f04d21323d573731d8737904a4c9b99fc527
Nicolas PASCAL <np.pascal@gmail.com>
2018-04-13 23:16:49 +0200

    djangocms-history for 0.15.x

--- cmsplugin_cascade/segmentation/cms_plugins.py
+++ cmsplugin_cascade/segmentation/cms_plugins.py
@@ -16,6 +16,8 @@ from cms.plugin_pool import plugin_pool
 from cmsplugin_cascade.fields import GlossaryField
 from cmsplugin_cascade.plugin_base import CascadePluginBase, TransparentContainer
 
+import json
+
 html_parser = HTMLParser()
 
 
@@ -82,6 +84,8 @@ class SegmentPlugin(TransparentContainer, CascadePluginBase):
     @classmethod
     def get_identifier(cls, obj):
         try:
+            if isinstance(obj.glossary, str):
+                obj.glossary=json.loads(obj.glossary)
             return mark_safe("<strong><em>{open_tag}</em></strong> {condition}".format(**obj.glossary))
         except KeyError:
             return ''
@@ -120,6 +124,8 @@ class SegmentPlugin(TransparentContainer, CascadePluginBase):
 
         request = context['request']
         edit_mode = self.in_edit_mode(request, placeholder)
+        if isinstance(instance.glossary, str):
+            instance.glossary=json.loads(instance.glossary)
         open_tag = instance.glossary.get('open_tag')
         if open_tag == 'if':
             template = conditionally_eval()
