70c7c19ade2e6e2a079f678027e75e21c6789925
Jacob Rief <jacob.rief@gmail.com>
2018-04-19 16:18:48 +0200

    Prepare for release 0.16

--- cmsplugin_cascade/__init__.py
+++ cmsplugin_cascade/__init__.py
@@ -19,6 +19,6 @@ Release logic:
 12. git commit -m 'Start with <version>'
 13. git push
 """
-__version__ = "0.15.5"
+__version__ = "0.16.dev0"
 
 default_app_config = 'cmsplugin_cascade.apps.CascadeConfig'
