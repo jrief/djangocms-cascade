92878b8d85d43f18d476377edd69d45b2f1768fd
Nicolas PASCAL <np.pascal@gmail.com>
2018-11-08 20:29:51 +0100

    Add variable environment needed for bs3demo

--- examples/manage.py
+++ examples/manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python                                                           
 import os
 import sys
 
@@ -6,6 +6,8 @@ sys.path.insert(0, os.path.abspath(os.path.pardir))
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
-
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bs3demo.settings')
-    execute_from_command_line(sys.argv)
+    if os.environ.get('BOOTSTRAP') == "3":
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bs3demo.settings')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bs4demo.settings')
+        execute_from_command_line(sys.argv)
