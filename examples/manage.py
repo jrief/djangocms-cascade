c57eb24ae5ee4e0fafb197e3c2939a931fd31f66
Haricot <np.pascal@gmail.com>
2018-11-09 22:57:27 +0100

    fix indent

--- examples/manage.py
+++ examples/manage.py
@@ -6,5 +6,5 @@ sys.path.insert(0, os.path.abspath(os.path.pardir))
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bs4demo.settings')
-        execute_from_command_line(sys.argv)
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bs4demo.settings')
+    execute_from_command_line(sys.argv)
