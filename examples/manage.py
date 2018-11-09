2adca41bdac22fb97960c6ef55142ba2cf6cf078
Nicolas PASCAL <np.pascal@gmail.com>
2018-11-09 20:50:34 +0100

    Use only bs4demo settings

--- examples/manage.py
+++ examples/manage.py
@@ -6,8 +6,5 @@ sys.path.insert(0, os.path.abspath(os.path.pardir))
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
-    if os.environ.get('BOOTSTRAP') == "3":
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bs3demo.settings')
-    else:
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bs4demo.settings')
         execute_from_command_line(sys.argv)
