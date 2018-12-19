385d504315b2cb09cc951f7be6554c92b8081ae3
Haricot <np.pascal@gmail.com>
2018-12-20 00:31:09 +0100

    optimize js

--- cmsplugin_cascade/static/cascade/js/admin/colorpicker.js
+++ cmsplugin_cascade/static/cascade/js/admin/colorpicker.js
@@ -1,26 +1,25 @@
 django.jQuery(function($) {
     'use strict';
 
-    function color_selector(id_glossary, id_picker) {
-        var data = $(id_glossary);
-        var thumb = 'thumb_' + id_picker;
-        if (data) {
-
-            if ($('#' + id_picker).length == '0') {
-                data.parent().append('<input id="' + id_picker + '" type="color" style="display:none;opacity:0;position:absolute;"/>');
-                data.parent().append('<span id="' + thumb + '" style="background-color:' + data.val() + '; position: absolute; top: 5px;  left: 5px; width: 18px; height:18px; border: 1px solid;" ></span>');
+    function color_selector(id_glossary) {
+        if (id_glossary) {
+            if (id_glossary.find('.color_picker').length == '0') {
+                var parent = id_glossary.parent();
+                parent.append('<input class="color_picker" type="color" style="display:none;opacity:0;position:absolute;"/>');
+                parent.append('<span class=".thumb_color_picker" style="background-color:' + id_glossary.val() + '; position: absolute; top: 5px;  left: 5px; width: 18px; height:18px; border: 1px solid;" ></span>');
             }
-            data.add("#" + thumb).on('click', function() {
-
-                $('#' + id_picker).on('change', $('#' + id_picker), function() {
-                    $('#' + thumb).css('background-color', $('#' + id_picker).val());
-                    data.val($('#' + id_picker).val());
+            id_glossary.add(".thumb_color_picker").on('click', function() {
+                var cp =  parent.find('.color_picker');
+                var thumb_cp = parent.find('.thumb_color_picker');
+                cp.on('change', function() {
+                thumb_cp.css('background-color', cp.val());
+                id_glossary.val(cp.val());
                 });
-                $('#' + id_picker).trigger('click');
+               cp.trigger('click');
             });
         };
     };
-    
-    color_selector('#id_glossary_extra_inline_styles\\:color_color', 'id_picker_color');
-    color_selector('#id_glossary_extra_inline_styles\\:background-color_color', 'id_picker_backgroundcolor');
+    $('.color_picker input').each(function() {
+        color_selector($(this));
+    });
 });
--- cmsplugin_cascade/static/cascade/js/admin/colorpickerext.js
+++ cmsplugin_cascade/static/cascade/js/admin/colorpickerext.js
@@ -2,38 +2,42 @@ django.jQuery(function($) {
     'use strict';
     $.getScript("/static/node_modules/a-color-picker/dist/acolorpicker.js", function() {});
 
-    function color_selector(id_glossary, id_picker) {
-        var bools = {};
-        var data = $(id_glossary);
-        var thumb = 'thumb_' + id_picker;
-        if (data) {
-            bools[id_picker] = true;
-            var $colordefault = data.val();
-            data.parent().append('<span id="' + thumb + '" style="background-color:' + data.val() + '; position: absolute; top: 5px;  left: 5px; width: 18px; height:18px; border: 1px solid;" ></span>');
-            data.add('#'+ thumb ).on('click', function() {
-                if (bools[id_picker]) {
-                    data.parent().append('<div id="' + id_picker + '"  acp-color="' + data.val() + '"  acp-show-rgb="no" acp-show-hsl="yes" acp-show-hex="yes" acp-show-alpha></div>');
-                    data.parent().append('<input id="undo_' + id_picker + '"  type="button" value="Undo">');
-                    AColorPicker.from('#' + id_picker).on('change', (picker, color) => {
-                        data.val(picker.color);
-                        $('#'+ thumb).css("background-color", data.val())
+    function color_selector(id_glossary) {
+        var is_not_open = false;
+        if (id_glossary) {
+            is_not_open = true;
+            var $colordefault = id_glossary.val();
+            var parent = id_glossary.parent();
+            parent.append('<span class="thumb_color_picker" style="background-color:' + id_glossary.val() + '; position: absolute; top: 5px;  left: 5px; width: 18px; height:18px; border: 1px solid;" ></span>');
+            id_glossary.add('.thumb_color_picker').on('click', function() {
+                if (is_not_open) {
+                    parent.append('<div class="acolor_picker"  acp-color="' + id_glossary.val() + '"  acp-show-rgb="no" acp-show-hsl="yes" acp-show-hex="yes" acp-show-alpha></div>');
+                    parent.append('<input class="undo_color_picker"  type="button" value="Undo">');
+                    var cp = id_glossary.parent().find('.acolor_picker');
+                    var thumb_cp = parent.find('.thumb_color_picker');
+                    AColorPicker.from(cp).on('change', (picker, color) => {
+                        id_glossary.val(picker.color);
+                        thumb_cp.css("background-color", id_glossary.val())
                     });
-                    bools[id_picker] = false;
-                    $('#undo_' + id_picker).on('click', function() {
-                        data.val($colordefault);
-                        $('#' + id_picker).detach();
-                        $('#'+ thumb).css("background-color", data.val());
-                        $('#undo_' + id_picker).detach();
-                        bools[id_picker] = true;
+                    is_not_open = false;
+                    var undo_cp = id_glossary.parent().find('.undo_color_picker')
+                    undo_cp.on('click', function() {
+                        id_glossary.val($colordefault);
+                        cp.detach();
+                        thumb_cp.css("background-color", id_glossary.val());
+                        undo_cp.find('.undo_color_picker').detach();
+                        is_not_open = true;
                     });
                 } else {
-                    $('#' + id_picker).detach();
-                    $('#undo_' + id_picker).detach();
-                    bools[id_picker] = true;
+                    id_glossary.parent().find('.acolor_picker').detach();
+                    id_glossary.parent().find('.undo_color_picker').detach();
+                    is_not_open = true;
                 }
             });
         }
     };
-    color_selector('#id_glossary_extra_inline_styles\\:color_color', 'id_picker_color');
-    color_selector('#id_glossary_extra_inline_styles\\:background-color_color', 'id_picker_backgroundcolor');
+    $('.color_picker input').each(function() {
+        color_selector($(this));
+    });
+
 });
