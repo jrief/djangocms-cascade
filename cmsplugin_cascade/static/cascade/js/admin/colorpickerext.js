67ad16d4b8508479a5755c5bd9768e7bdc2040ff
Haricot <np.pascal@gmail.com>
2018-12-19 14:59:57 +0100

    modif style color picker

--- cmsplugin_cascade/static/cascade/js/admin/colorpickerext.js
+++ cmsplugin_cascade/static/cascade/js/admin/colorpickerext.js
@@ -2,33 +2,34 @@ django.jQuery(function($) {
     'use strict';
     $.getScript("/static/node_modules/a-color-picker/dist/acolorpicker.js", function() {});
 
-    function color_selector(data, id_picker) {
+    function color_selector(id_glossary, id_picker) {
         var bools = {};
-        data = $(data);
+        var data = $(id_glossary);
+        var thumb = 'thumb_' + id_picker;
         if (data) {
-            bools[data] = true;
+            bools[id_picker] = true;
             var $colordefault = data.val();
-            data.parent().append('<span id="thumb' + id_picker + '" style="background-color:' + data.val() + '; position: absolute; top: 5px;  left: 5px; width: 18px; height:18px; border: 1px solid;" ></span>');
-            data.on('click', function() {
-                if (bools[data]) {
-                    data.parent().append('<div id="' + id_picker + '"  acp-color="' + data.val() + '"  acp-show-rgb="yes" acp-show-hsl="yes" acp-show-hex="yes" acp-show-alpha></div>');
+            data.parent().append('<span id="' + thumb + '" style="background-color:' + data.val() + '; position: absolute; top: 5px;  left: 5px; width: 18px; height:18px; border: 1px solid;" ></span>');
+            data.add('#'+ thumb ).on('click', function() {
+                if (bools[id_picker]) {
+                    data.parent().append('<div id="' + id_picker + '"  acp-color="' + data.val() + '"  acp-show-rgb="no" acp-show-hsl="yes" acp-show-hex="yes" acp-show-alpha></div>');
                     data.parent().append('<input id="undo_' + id_picker + '"  type="button" value="Undo">');
                     AColorPicker.from('#' + id_picker).on('change', (picker, color) => {
                         data.val(picker.color);
-                        $('#thumb' + id_picker).css("background-color", data.val())
+                        $('#'+ id_picker ).css("background-color", data.val())
                     });
-                    bools[data] = false;
+                    bools[id_picker] = false;
                     $('#undo_' + id_picker).on('click', function() {
                         data.val($colordefault);
                         $('#' + id_picker).detach();
                         $('#thumb' + id_picker).css("background-color", data.val());
                         $('#undo_' + id_picker).detach();
-                        bools[data] = true;
+                        bools[id_picker] = true;
                     });
                 } else {
                     $('#' + id_picker).detach();
                     $('#undo_' + id_picker).detach();
-                    bools[data] = true;
+                    bools[id_picker] = true;
                 }
             });
         }
