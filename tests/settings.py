605189959f771343b7d87a42a2c5f85175c9aece
Jacob Rief <jacob.rief@gmail.com>
2019-05-22 00:08:24 +0200

    adopt tests to Django-2.0

--- tests/settings.py
+++ tests/settings.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 
-from django.core.urlresolvers import reverse_lazy
+from django.urls import reverse_lazy
 from django.utils.text import format_lazy
 from cmsplugin_cascade.extra_fields.config import PluginExtraFieldsConfig
 
