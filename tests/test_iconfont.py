5dd88276ef1efe7cfd02998a9e79b0deb5386712
Haricot <np.pascal@gmail.com>
2019-06-28 13:56:37 +0200

    update test_iconfont.py

--- tests/test_iconfont.py
+++ tests/test_iconfont.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 import os
 from bs4 import BeautifulSoup
 import json
-from django import VERSION as DJANGO_VERSION
+
 from django.contrib.auth import get_user_model
 from django.test.client import Client
 from django.urls import reverse, resolve
@@ -17,12 +17,11 @@ from cms.utils.plugins import build_plugin_tree
 
 from cmsplugin_cascade import app_settings
 from cmsplugin_cascade.models import IconFont
-from cmsplugin_cascade.bootstrap3.container import BootstrapContainerPlugin
+from cmsplugin_cascade.bootstrap4.container import BootstrapContainerPlugin
 from cmsplugin_cascade.icon.cms_plugins import FramedIconPlugin
 from .test_base import CascadeTestCase
 
-BS3_BREAKPOINT_KEYS = list(tp[0] for tp in app_settings.CMSPLUGIN_CASCADE['bootstrap3']['breakpoints'])
-
+BS4_BREAKPOINT_KEYS=['xs', 'sm', 'md', 'lg','xl']
 
 class IconFontTestCase(CascadeTestCase):
     client = Client()
@@ -59,11 +58,7 @@ class IconFontTestCase(CascadeTestCase):
             self.assertEqual(resolver_match.url_name, 'cmsplugin_cascade_iconfont_change')
 
             # check the content of the uploaded file
-            if DJANGO_VERSION < (2,0):
-                object_id = resolver_match.args[0]
-            else:
-                object_id = resolver_match.kwargs['object_id']
-            icon_font = IconFont.objects.get(pk=object_id)
+            icon_font = IconFont.objects.get(pk=resolver_match.kwargs['object_id'])
             self.assertEqual(icon_font.identifier, "Fontellico")
             self.assertEqual(icon_font.config_data['name'], 'fontelico')
             self.assertEqual(len(icon_font.config_data['glyphs']), 34)
@@ -71,7 +66,7 @@ class IconFontTestCase(CascadeTestCase):
             # check if the uploaded fonts are rendered inside Preview Icons
             response = self.client.get(response.url)
             self.assertEqual(response.status_code, 200)
-            soup = BeautifulSoup(response.content, features='lxml')
+            soup = BeautifulSoup(response.content, 'lxml')
             preview_iconfont = soup.find('div', class_="preview-iconfont")
             icon_items = preview_iconfont.ul.find_all('li')
             self.assertEqual(len(icon_items), 34)
@@ -84,14 +79,15 @@ class IconFontTestCase(CascadeTestCase):
 
             # create container
             container_model = add_plugin(self.placeholder, BootstrapContainerPlugin, 'en',
-                                         glossary={'breakpoints': BS3_BREAKPOINT_KEYS})
+                                         glossary={'breakpoints': BS4_BREAKPOINT_KEYS})
             container_plugin = container_model.get_plugin_class_instance()
             self.assertIsInstance(container_plugin, BootstrapContainerPlugin)
 
             # add icon as child to this container
-            glossary = {"font_size": "10em", "color": ["","#88258e"], "background_color": ["on","#c8ffcd"],
-                        "symbol": "emo-wink", "icon_font": icon_font.pk,
-                        "border_radius":"50%","border":["","solid","#000000"]}
+            glossary = {"font_size": "10em", "color": ["#88258e", ""],  "background_color":[ "#c8ffcd",
+                       "f"  ], "icon_font":{  "model":"cmsplugin_cascade.iconfont",   "pk":1 },
+                      "symbol":"emo-wink","font_size":"1em", "text_align":"", "border":[ "0px", "solid", "#000000"],
+                      "border_radius":"50%"}
             icon_model = add_plugin(self.placeholder, FramedIconPlugin, 'en', target=container_model,
                                     glossary=glossary)
             icon_plugin = icon_model.get_plugin_class_instance()
@@ -100,8 +96,8 @@ class IconFontTestCase(CascadeTestCase):
             # render the plugins
             plugin_list = [container_model, icon_model]
             build_plugin_tree(plugin_list)
-            html = self.get_html(container_model, self.get_request_context())
-            soup = BeautifulSoup(html, features='lxml')
+            html = self.get_html(container_model, self.get_request_context()) 
+            soup = BeautifulSoup(html, 'lxml')
 
             # look for the icon symbol
             style = soup.find('span', class_='icon-emo-wink').attrs['style'].split(';')
@@ -111,7 +107,7 @@ class IconFontTestCase(CascadeTestCase):
             # look for the CSS file
             response = self.client.get(container_model.placeholder.page.get_absolute_url() + '?edit')
             self.assertEqual(response.status_code, 200)
-            soup = BeautifulSoup(response.content, features='lxml')
+            soup = BeautifulSoup(response.content, 'lxml')
             links = soup.head.find_all('link')
             for link in links:
                 if link.attrs['href'].endswith('fontelico.css'):
