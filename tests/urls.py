5b9a1c1137c4f8c0f3717d8e38eb6c88d03a67de
Haricot <np.pascal@gmail.com>
2019-06-28 22:43:48 +0200

    update urls.py for django{111,2}

--- tests/urls.py
+++ tests/urls.py
@@ -1,8 +1,19 @@
-from django.conf.urls import url, include
+import django
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
-urlpatterns = i18n_patterns(
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('cms.urls')),
-)
+if django.get_version() < "2.0.0":    
+    from django.conf.urls import url, include
+
+    urlpatterns = i18n_patterns(
+        url(r'^admin/', admin.site.urls),
+        url(r'^', include('cms.urls')),
+    )
+
+if django.get_version() >= "2.0.0":    
+    from django.urls import path, include
+
+    urlpatterns = i18n_patterns(
+        path('admin/', admin.site.urls),
+        path('', include('cms.urls')),
+    )
