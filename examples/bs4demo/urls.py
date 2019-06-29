347faf134d3007f3b252426c23747f817f9337df
Haricot <np.pascal@gmail.com>
2019-06-29 02:37:32 +0200

    update examples django 2

--- examples/bs4demo/urls.py
+++ examples/bs4demo/urls.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
-from django.conf.urls import url, include
+from django.urls import include, path, re_path
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
+
 
 class CascadeDemoView(TemplateView):
     template_name = 'bs4demo/strides.html'
@@ -12,9 +13,9 @@ class CascadeDemoView(TemplateView):
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/select2/', include('django_select2.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^cascade/$', CascadeDemoView.as_view()),
-    url(r'^', include('cms.urls')),
+    path('admin/select2/', include('django_select2.urls')),
+    path('admin/', admin.site.urls  ),
+    path('cascade/', CascadeDemoView.as_view()),
+    path('', include('cms.urls')),
 ]
 urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
