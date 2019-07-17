a6e53ed0bc446b300553485ce388e1bd72420284
Jacob Rief <jacob.rief@gmail.com>
2019-07-17 17:51:12 +0200

    separate conftest for Bootstrap-4

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,7 @@
 import pytest
 from django.contrib import admin
-from cms.api import create_page, add_plugin
+from cms.api import create_page
 from cmsplugin_cascade.models import CascadePage
-from cmsplugin_cascade.models import CascadeElement
-from cmsplugin_cascade.bootstrap4.container import BootstrapContainerPlugin, BootstrapRowPlugin
 
 
 @pytest.fixture
@@ -26,27 +24,3 @@ def cms_page():
 def cms_placeholder(cms_page):
     placeholder = cms_page.placeholders.get(slot='Main Content')
     return placeholder
-
-
-@pytest.fixture
-@pytest.mark.django_db
-def bootstrap_container(admin_site, cms_placeholder):
-    # add a Bootstrap Container Plugin
-    glossary = {'breakpoints': ['xs', 'sm', 'md', 'lg', 'xl'], 'fluid': ''}
-    container_model = add_plugin(cms_placeholder, BootstrapContainerPlugin, 'en', glossary=glossary)
-    assert isinstance(container_model, CascadeElement)
-    container_plugin = container_model.get_plugin_class_instance(admin_site)
-    assert isinstance(container_plugin, BootstrapContainerPlugin)
-    return container_plugin, container_model
-
-
-@pytest.fixture
-@pytest.mark.django_db
-def bootstrap_row(admin_site, bootstrap_container):
-    # add a Bootstrap Row Plugin to the given container
-    container_plugin, container_model = bootstrap_container
-    row_model = add_plugin(container_model.placeholder, BootstrapRowPlugin, 'en', target=container_model)
-    assert isinstance(row_model, CascadeElement)
-    row_plugin = row_model.get_plugin_class_instance()
-    assert isinstance(row_plugin, BootstrapRowPlugin)
-    return row_plugin, row_model
