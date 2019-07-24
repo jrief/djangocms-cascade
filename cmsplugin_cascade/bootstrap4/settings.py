974f832c417d9727ff7dc284287e76d1e0e76690
Jacob Rief <jacob.rief@gmail.com>
2019-07-24 21:55:44 +0200

    Refactored FramedIconPlugin into bootstrap-4 subproject

--- cmsplugin_cascade/bootstrap4/settings.py
+++ cmsplugin_cascade/bootstrap4/settings.py
@@ -9,7 +9,7 @@ from .grid import Breakpoint, Bound
 assert DJANGO_VERSION >= (1, 11), "djangocms-cascade with Bootstrap-4 requires at least Django-1.11"
 
 
-CASCADE_PLUGINS = ['accordion', 'buttons', 'card', 'carousel', 'container', 'embeds', 'image', 'jumbotron',
+CASCADE_PLUGINS = ['accordion', 'buttons', 'card', 'carousel', 'container', 'embeds', 'icon', 'image', 'jumbotron',
                    'picture', 'tabs']
 if 'cms_bootstrap' in settings.INSTALLED_APPS:
     CASCADE_PLUGINS.append('secondary_menu')
