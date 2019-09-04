ba6d491d3abb0635d6039b341564a01d5fbbe35f
Jacob Rief <jacob.rief@gmail.com>
2019-09-04 21:45:23 +0200

    update docstrings

--- cmsplugin_cascade/extra_fields/config.py
+++ cmsplugin_cascade/extra_fields/config.py
@@ -11,13 +11,19 @@ class PluginExtraFieldsConfig(object):
     ```
     *Start › django CMS Cascade › Custom CSS classes and styles › PluginExtraFields*
     ```
-    :param allow_id_tag: If ``True``, allows to set the ``id`` attribute in HTML elements.
-    :param css_classes: A dictionary containing:
-    ``class_names`` a comma separated string of allowed class names.
-    ``multiple`` a Boolean indicating if more multiple classes are allowed concurrently.
-    :param inline_styles: A dictionary containing:
-    :param allow_override: If ``True``, allows to override this configuration using the admin's
-    backend interface.
+    :param allow_id_tag:
+        If ``True``, allows to set the ``id`` attribute in HTML elements.
+
+    :param css_classes:
+        A dictionary containing:
+          ``class_names`` a comma separated string of allowed class names.
+          ``multiple`` a Boolean indicating if more multiple classes are allowed concurrently.
+
+    :param inline_styles:
+        A dictionary containing:
+
+    :param allow_override:
+        If ``True``, allows to override this configuration using the admin's backend interface.
     """
     def __init__(self, allow_id_tag=False, css_classes=None, inline_styles=None, allow_override=True):
         self.allow_id_tag = allow_id_tag
