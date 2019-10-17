8af7f4ac64feb6a2ca26bded3c08193e07135899
Nicolas PASCAL <np.pascal@gmail.com>
2019-10-17 12:02:20 +0200

    Update cms_plugins.py

--- cmsplugin_cascade/icon/cms_plugins.py
+++ cmsplugin_cascade/icon/cms_plugins.py
@@ -23,7 +23,7 @@ class SimpleIconPlugin(IconPluginMixin, LinkPluginBase):
     @classmethod
     def get_css_classes(cls, obj):
         css_classes = cls.super(SimpleIconPlugin, cls).get_css_classes(obj)
-        if cls.plugin_class.__class__ == 'BootstrapNavItemsPlugin':
+        if cls == 'BootstrapNavItemsPlugin':
             css_classes.insert(0,'nav-link navbar-text')
         return css_classes
 
