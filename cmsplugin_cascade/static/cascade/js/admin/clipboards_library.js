771fa37c4b77ccad79ce2948d6d32a40a8f6d136
Nicolas PASCAL <np.pascal@gmail.com>
2019-10-21 15:06:02 +0200

    Update clipboards_library.js

--- cmsplugin_cascade/static/cascade/js/admin/clipboards_library.js
+++ cmsplugin_cascade/static/cascade/js/admin/clipboards_library.js
@@ -7,9 +7,9 @@ function inIframe() {
 }
 
 if (inIframe()) {
-    CMS = window.parent.CMS;
+    var CMS = window.parent.CMS;
 } else {
-    CMS = window.CMS
+    var CMS = window.CMS;
 }
 
 function LocalStorage_or_AppendFolderRender(element, url, timestamp) {
@@ -23,8 +23,8 @@ function LocalStorage_or_AppendFolderRender(element, url, timestamp) {
         timestamp_storage = sessionStorage.getItem(timestamp_storage_folder);
         sessionStorage.setItem("folder_current_id", idFolder);
 
-        if (sessionStorage.getItem(timestamp_storage_folder) === timestamp.toString() && sessionStorage.getItem('HTML_FOLDER_CLIPS_' + id_folder !== null)) {
-            data_html = sessionStorage.getItem('HTML_FOLDER_CLIPS_' + idFolder);
+        if (sessionStorage.getItem(timestamp_storage_folder) === timestamp.toString() && sessionStorage.getItem("HTML_FOLDER_CLIPS_" + id_folder !== null)) {
+            data_html = sessionStorage.getItem("HTML_FOLDER_CLIPS_" + idFolder);
             $(".clips-wrapper").html(data_html);
         } else {
             var token = "{ 'csrfmiddlewaretoken': '" + CMS.config.csrf + "' }";
@@ -36,13 +36,13 @@ function LocalStorage_or_AppendFolderRender(element, url, timestamp) {
             };
 
             $.ajax({
-                type: 'POST',
-                url: '/cascade_libclips_folder/' + idFolder + '/',
+                type: "POST",
+                url: "/cascade_libclips_folder/" + idFolder + "/",
                 post: token,
                 data: data,
                 success: function(response) {
                     sessionStorage.setItem(timestamp_storage_folder, timestamp);
-                    sessionStorage.setItem("HTML_FOLDER_CLIPS_é + idFolder, response);
+                    sessionStorage.setItem("HTML_FOLDER_CLIPS_" + idFolder, response);
                     $(".clips-wrapper").html(response);
                 }
             });
@@ -55,10 +55,10 @@ function LocalStorage_or_AppendFolderRender(element, url, timestamp) {
 }
 
 function ajaxCopyToClipboard(event, that, url, paramater) {
-    var data_post = JSON.parse($(that).attr('data-post'));
+    var data_post = JSON.parse($(that).attr("data-post"));
     var csrf = data_post.csrfmiddlewaretoken;
     var paramater;
-    var token = '{ "csrfmiddlewaretoken": "' + csrf + '" }';
+    var token = "{ 'csrfmiddlewaretoken': '" + csrf + "' }";
     var data = {
         paramater: paramater,
         placeholder_id: 1,
@@ -66,7 +66,7 @@ function ajaxCopyToClipboard(event, that, url, paramater) {
         target_language: "en"
     };
     $.ajax({
-        type: 'POST',
+        type: "POST",
         url: url,
         post: token,
         data: data,
@@ -75,15 +75,15 @@ function ajaxCopyToClipboard(event, that, url, paramater) {
         if (inIframe()) {
             CMS.API.Clipboard._enableTriggers();
             stat = $.extend({}, data, response.data);
-            CMS.API.StructureBoard.invalidateState('MOVE', stat);
+            CMS.API.StructureBoard.invalidateState("MOVE", stat);
             CMS.API.Clipboard.ui.pluginsList.html(response.data.html);
-            CMS.API.Clipboard.ui.triggers.trigger('click.cms.clipboard');
+            CMS.API.Clipboard.ui.triggers.trigger("click.cms.clipboard");
             CMS.API.StructureBoard._loadedStructure = false;
             CMS.API.StructureBoard.show();
             //CMS.API.Helpers.reloadBrowser();
         } else {
-            $('#modal_dialog').removeClass('d-none')
-            $('#modal_dialog').modal('show');
+            $("#modal_dialog").removeClass("d-none")
+            $("#modal_dialog").modal("show");
         }
     });
 }
