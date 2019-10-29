da4d0e173db864b181bcdf51fdd7cb08d8f17fcb
Nicolas PASCAL <np.pascal@gmail.com>
2019-10-29 11:52:21 +0100

    Added menu item modal  link: Cascade Theme

--- cmsplugin_cascade/cms_toolbars.py
+++ cmsplugin_cascade/cms_toolbars.py
@@ -1,8 +1,10 @@
 from django.utils.translation import ugettext_lazy as _
+from django.urls import reverse
 from cms.extensions.toolbar import ExtensionToolbar
 from cms.toolbar_pool import toolbar_pool
 from cms.toolbar.items import Break
-from cms.cms_toolbars import PAGE_MENU_SECOND_BREAK
+from cms.toolbar_base import CMSToolbar
+from cms.cms_toolbars import PAGE_MENU_SECOND_BREAK, USER_SETTINGS_BREAK
 from cmsplugin_cascade.models import CascadePage
 
 
@@ -19,3 +21,21 @@ class CascadePageToolbar(ExtensionToolbar):
                 position = current_page_menu.find_first(Break, identifier=PAGE_MENU_SECOND_BREAK)
                 disabled = not self.toolbar.edit_mode_active
                 current_page_menu.add_modal_item(_("Extra Page Fields"), position=position, url=url, disabled=disabled)
+        
+        
+@toolbar_pool.register
+class CascadeToolbar(CMSToolbar):
+
+    def populate(self):
+        if getattr(settings, 'CASCADE_THEME', None):
+            admin_menu = self.toolbar.get_or_create_menu(
+                ADMIN_MENU_IDENTIFIER, _('Site'))
+            position_user_set= admin_menu.find_first(Break, identifier=USER_SETTINGS_BREAK)
+            position_theme = admin_menu.add_break('THEME', position_user_set)
+            position_theme = admin_menu.find_first(Break, identifier=USER_SETTINGS_BREAK)
+            
+            admin_menu.add_modal_item( 
+                name='Theme',
+                url=reverse('cascade_theme'),
+                position=position_theme,
+                )
