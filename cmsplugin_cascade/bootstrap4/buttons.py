f768ee42c70e9f8fde3e391c177e1cb13a69b818
Nicolas PASCAL <np.pascal@gmail.com>
2019-11-01 20:27:34 +0100

    sass_processor logic for theme button

--- cmsplugin_cascade/bootstrap4/buttons.py
+++ cmsplugin_cascade/bootstrap4/buttons.py
@@ -1,4 +1,5 @@
 from django import VERSION as DJANGO_VERSION
+from django.conf import settings
 from django.forms import widgets
 from django.forms.fields import BooleanField, CharField, ChoiceField, MultipleChoiceField
 from django.utils.html import format_html
@@ -119,7 +120,13 @@ class BootstrapButtonMixin(IconPluginMixin):
     ring_plugin = 'ButtonMixin'
 
     class Media:
-        css = {'all': ['cascade/css/admin/bootstrap4-buttons.css', 'cascade/css/admin/iconplugin.css']}
+        if getattr(settings, 'CASCADE_THEME', None):
+            from sass_processor.processor import sass_processor
+            css = {
+                'all': [sass_processor('cascade/css/admin/bootstrap4-buttons.scss'), 'cascade/css/admin/iconplugin.css'],
+            }
+        else:
+            css = {'all': ['cascade/css/admin/bootstrap4-buttons.css', 'cascade/css/admin/iconplugin.css']}
         js = ['admin/js/jquery.init.js', 'cascade/js/admin/buttonmixin.js']
 
     def render(self, context, instance, placeholder):
