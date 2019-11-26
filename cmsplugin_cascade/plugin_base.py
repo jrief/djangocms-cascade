57e1a107393499a1d41444790731e8a22c6a32b6
Nicolas PASCAL <np.pascal@gmail.com>
2019-11-26 21:01:53 +0100

    Update plugin_base.py set fielfset

--- cmsplugin_cascade/plugin_base.py
+++ cmsplugin_cascade/plugin_base.py
@@ -314,8 +314,7 @@ class CascadePluginBase(metaclass=CascadePluginBaseMetaclass):
     def get_form(self, request, obj=None, **kwargs):
         form = kwargs.get('form', self.form)
         assert issubclass(form, EntangledModelFormMixin), "Form must inherit from EntangledModelFormMixin"
-        if self.fieldsets:
-            self.fieldsets = tuple(gen_separate_fieldsets(form))
+        self.fieldsets = tuple(gen_separate_fieldsets(form))
         if issubclass(form, ModelForm):
             kwargs['form'] = form
         else:
