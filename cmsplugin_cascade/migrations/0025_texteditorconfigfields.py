49f1e1b8483af9e13f0c5d0dd99306f8d1cf1ff9
Nicolas PASCAL <np.pascal@gmail.com>
2020-01-13 12:18:18 +0100

    fix test missing migrations texteditorconfigfields

--- cmsplugin_cascade/migrations/0025_texteditorconfigfields.py
+++ cmsplugin_cascade/migrations/0025_texteditorconfigfields.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=50, verbose_name='Name')),
-                ('element_type', models.CharField(choices=[('p', 'p'), ('h1', 'h1'), ('h2', 'h2'), ('h3', 'h3'), ('h4', 'h4'), ('h5', 'h5'), ('h6', 'h6'), ('pre', 'pre'), ('address', 'address'), ('div', 'div')], max_length=12, verbose_name='Element Type')),
+                ('element_type', models.CharField(choices=[('p', 'p'), ('h1', 'h1'), ('h2', 'h2'), ('h3', 'h3'), ('h4', 'h4'), ('h5', 'h5'), ('h6', 'h6'), ('pre', 'pre'), ('address', 'address'), ('div', 'div'), ('span', 'span')], max_length=12, verbose_name='Element Type')),
                 ('css_classes', models.CharField(help_text='Freely selectable CSS classnames for this Text-Editor Style, separated by spaces.', max_length=250, verbose_name='CSS classes')),
             ],
             options={
