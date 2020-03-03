f72dc63662e2097f6f7d023efe0058d9f1ec4988
Haricot <np.pascal@gmail.com>
2020-03-03 18:44:54 +0100

    add `class ManageNestedFormMixin`

--- cmsplugin_cascade/bootstrap4/accordion.py
+++ cmsplugin_cascade/bootstrap4/accordion.py
@@ -4,15 +4,16 @@ from django.utils.translation import ungettext_lazy, ugettext_lazy as _
 from django.utils.safestring import mark_safe 
 from django.utils.text import Truncator
 from django.utils.html import escape
-from entangled.forms import EntangledModelFormMixin
+from entangled.forms import EntangledModelFormMixin, EntangledFormField, EntangledForm
 from cms.plugin_pool import plugin_pool
-from cmsplugin_cascade.forms import ManageChildrenFormMixin
+from cmsplugin_cascade.forms import ManageChildrenFormMixin, ManageNestedFormMixin
 from cmsplugin_cascade.plugin_base import TransparentWrapper, TransparentContainer
 from cmsplugin_cascade.widgets import NumberInputWidget
 from .plugin_base import BootstrapPluginBase
-from cmsplugin_cascade.helpers import used_compact_form, entangled_nested
+from django.forms import formset_factory
 
-class AccordionFormMixin(ManageChildrenFormMixin, EntangledModelFormMixin):
+
+class AccordionForm(EntangledForm):
     num_children = IntegerField(
         min_value=1,
         initial=1,
@@ -35,12 +36,12 @@ class AccordionFormMixin(ManageChildrenFormMixin, EntangledModelFormMixin):
          help_text=_("Start with the first card open.")
     )
 
-    if used_compact_form:
-        entangled_nested(num_children, close_others,first_is_open, data_nested='accordion')
+class AccordionFormMixin( ManageNestedFormMixin, EntangledModelFormMixin):
+    accordion_nested = EntangledFormField(AccordionForm)
 
     class Meta:
-        untangled_fields = ['num_children']
-        entangled_fields = {'glossary': ['close_others', 'first_is_open']}
+        untangled_fields = ['accordion_nested.num_children']
+        entangled_fields = {'glossary': ['accordion_nested']}
 
 
 class BootstrapAccordionPlugin(TransparentWrapper, BootstrapPluginBase):
@@ -62,20 +63,20 @@ class BootstrapAccordionPlugin(TransparentWrapper, BootstrapPluginBase):
     def render(self, context, instance, placeholder):
         context = self.super(BootstrapAccordionPlugin, self).render(context, instance, placeholder)
         context.update({
-            'close_others': instance.glossary.get('close_others', True),
-            'first_is_open': instance.glossary.get('first_is_open', True),
+            'close_others': instance.glossary['accordion_nested']['close_others'],
+            'first_is_open': instance.glossary['accordion_nested']['first_is_open'],
         })
         return context
 
-    def save_model(self, request, obj, form, change):
-        wanted_children = int(form.cleaned_data.get('num_children'))
+    def save_model(self, request, obj, form, change): 
+        wanted_children = int(form.cleaned_data.get('accordion_nested.num_children'))
         super().save_model(request, obj, form, change)
         self.extend_children(obj, wanted_children, BootstrapAccordionGroupPlugin)
 
 plugin_pool.register_plugin(BootstrapAccordionPlugin)
 
 
-class AccordionGroupFormMixin(EntangledModelFormMixin):
+class AccordionGroupForm(EntangledForm):
     heading = CharField(
         label=_("Heading"),
         widget=widgets.TextInput(attrs={'size': 80}),
@@ -88,14 +89,15 @@ class AccordionGroupFormMixin(EntangledModelFormMixin):
          help_text=_("Add standard padding to card body."),
     )
 
-    if used_compact_form:
-        entangled_nested(heading,body_padding,  data_nested='accordion_group')
+    def clean_heading(self):
+        return escape(self.cleaned_data.get('heading'))
+
+
+class AccordionGroupFormMixin( ManageNestedFormMixin, EntangledModelFormMixin):
+    accordion_nested = EntangledFormField(AccordionGroupForm)
 
     class Meta:
-        entangled_fields = {'glossary': ['heading', 'body_padding']}
-
-    def clean_heading(self):
-        return escape(self.cleaned_data['heading'])
+        entangled_fields = {'glossary': ['accordion_nested']}
 
 
 class BootstrapAccordionGroupPlugin(TransparentContainer, BootstrapPluginBase):
@@ -108,14 +110,14 @@ class BootstrapAccordionGroupPlugin(TransparentContainer, BootstrapPluginBase):
 
     @classmethod
     def get_identifier(cls, instance):
-        heading = instance.glossary.get('heading', '')
+        heading = instance.glossary.get('accordion_nested', {}).get('heading' , True)
         return Truncator(heading).words(3, truncate=' ...')
 
     def render(self, context, instance, placeholder):
         context = self.super(BootstrapAccordionGroupPlugin, self).render(context, instance, placeholder)
         context.update({
-            'heading': mark_safe(instance.glossary.get('heading', '')),
-            'no_body_padding': not instance.glossary.get('body_padding', True),
+            'heading': mark_safe(instance.glossary.get('accordion_nested', {}).get('heading' , True)),
+            'no_body_padding': not instance.glossary.get('accordion_nested', {}).get('no_body_padding' , True),
         })
         return context
 
