276ef6783a61622757dc5ee2078d073cc4db0b42
Jacob Rief <jacob.rief@gmail.com>
2020-03-11 15:30:23 +0100

    on delete, set foreign key to created_by to NULL

--- cmsplugin_cascade/migrations/0028_cascade_clipboard.py
+++ cmsplugin_cascade/migrations/0028_cascade_clipboard.py
@@ -7,10 +7,6 @@ import django.db.models.deletion
 import django.utils.timezone
 
 
-def get_default_user():
-    return get_user_model().objects.filter(is_superuser=True).first().pk
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -28,7 +24,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='cascadeclipboard',
             name='created_by',
-            field=models.ForeignKey(editable=False, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL, verbose_name='Created by'),
+            field=models.ForeignKey(editable=False, null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL, verbose_name='Created by'),
             preserve_default=False,
         ),
         migrations.AddField(
--- cmsplugin_cascade/models.py
+++ cmsplugin_cascade/models.py
@@ -248,7 +248,7 @@ class CascadeClipboard(models.Model):
     created_by = models.ForeignKey(
         get_user_model(),
         verbose_name=_("Created by"),
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         editable=False,
         null=True,
     )
