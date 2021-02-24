dddf1ed44b4ae9d919801db78bbf533509f53dfe
Jacob Rief - Django-Migration <jacob.rief@uibk.ac.at>
2021-02-24 11:57:19 +0100

    attempt to fix #390

--- cmsplugin_cascade/plugin_base.py
+++ cmsplugin_cascade/plugin_base.py
@@ -392,7 +392,8 @@ class CascadePluginBase(metaclass=CascadePluginBaseMetaclass):
             context.update(
                 ring_plugin=self.ring_plugin,
             )
-        context['empty_form'] = not context['adminform'].form._meta.entangled_fields.get('glossary')
+        context['empty_form'] = not (context['adminform'].form._meta.entangled_fields.get('glossary') or
+                                     context['adminform'].form._meta.untangled_fields)
         return super().render_change_form(request, context, add, change, form_url, obj)
 
     def in_edit_mode(self, request, placeholder):
